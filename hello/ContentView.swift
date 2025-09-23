ff46caf8723368a1709b9ead7dd665b4980b7bcf
sesquivel15 <123580665+sesquivel15@users.noreply.github.com>
2025-09-23 00:13:35 -0700

    Initial Commit

---
+++ hello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hello
+//
+//  Created by Stephanie Esquivel on 9/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by Stephanie Esquivel on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
